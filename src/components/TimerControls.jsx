1bb2adcfc3eed66dc6abc6d2f8dcdbb6de327059
braiancalot <calot665@hotmail.com>
2026-01-31 07:44:13 -0300

    refactor: improve UI

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -11,16 +11,15 @@ export function TimerControls({
     <div className="flex w-full pb-8 gap-4 items-center justify-center">
       {!isRunning && hasTime && (
         <button
-          className="px-5 py-3 border border-neutral-600 text-white rounded-lg text-sm transition-all hover:bg-neutral-900 active:scale-95 active:bg-neutral-700"
+          className="px-5 py-2 min-w-20 border border-neutral-600 outline-hidden text-white rounded-lg text-sm transition-all hover:bg-neutral-900 active:scale-95 active:bg-neutral-700"
           onClick={onReset}
-          disabled={!hasTime}
         >
           Reset
         </button>
       )}
 
       <button
-        className={`px-5 py-3 border border-transparent text-white rounded-lg active:scale-95 text-sm font-medium transition-all ${
+        className={`px-5 py-2 border min-w-20 border-transparent outline-hidden text-white rounded-lg active:scale-95 text-sm font-medium transition-all ${
           isRunning
             ? "bg-cyan-600 hover:bg-cyan-700 active:bg-cyan-800"
             : "bg-green-600 hover:bg-green-700 active:bg-green-800"
--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -2,12 +2,16 @@
 
 import { formatTime } from "@/lib/timer";
 
+function hasHours(hours) {
+  hours !== "00";
+}
+
 export function TimerDisplay({ time }) {
   const { hours, minutes, seconds, milliseconds } = formatTime(time);
 
   return (
-    <div className="flex text-6xl md:text-8xl font-medium items-center justify-center tabular-nums">
-      {hours !== "00" && (
+    <div className="flex text-6xl md:text-8xl font-medium items-center justify-center">
+      {hasHours(hours) && (
         <>
           <span>{hours}</span>
           <span className="opacity-50">:</span>
