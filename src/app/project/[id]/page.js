5c6d501d11e35f3fbeaf37330d8916f9866fe7f2
braiancalot <calot665@hotmail.com>
2026-02-09 18:51:38 -0300

    fix: improve rename input

--- src/app/project/[id]/page.js
+++ src/app/project/[id]/page.js
@@ -22,10 +22,22 @@ export default function ProjectPage({ params }) {
 
   if (isLoading) return null;
 
+  function handleStartRename() {
+    setIsRenaming(true);
+  }
+
   function handleRename() {
     if (!newName) return;
+
     projectRepository.rename({ id, name: newName });
+    setIsRenaming(false);
+    setNewName("");
     window.location.reload();
+  }
+
+  function handleCancel() {
+    setIsRenaming(false);
+    setNewName("");
   }
 
   if (!project) {
@@ -38,19 +50,21 @@ export default function ProjectPage({ params }) {
 
   return (
     <main className="w-full h-dvh flex flex-col items-center justify-center px-8">
-      <header className="w-full h-16 flex items-center justify-between">
-        <div className="flex items-center gap-4 justify-start">
+      <header className="w-full h-16 flex items-center justify-between gap-4">
+        <div className="flex items-center gap-4 justify-start ">
           <Link href="/" className="text-lg">
             ←
           </Link>
 
           {isRenaming ? (
-            <input
-              value={newName}
-              onChange={(event) => setNewName(event.target.value)}
-              className="border border-teal-500 rounded text-white py-2 px-3 outline-none w-24 md:w-auto"
-              autoFocus
-            />
+            <form onSubmit={handleRename} className="w-auto">
+              <input
+                value={newName}
+                onChange={(event) => setNewName(event.target.value)}
+                className="border border-teal-500 rounded text-white py-2 px-3 outline-none w-full"
+                autoFocus
+              />
+            </form>
           ) : (
             <h1 className="text-lg font-medium">{project.name}</h1>
           )}
@@ -58,34 +72,18 @@ export default function ProjectPage({ params }) {
 
         {isRenaming ? (
           <div className="flex gap-4">
-            <button
-              className="cursor-pointer"
-              onClick={() => {
-                setIsRenaming(false);
-                setNewName("");
-              }}
-            >
+            <button className="cursor-pointer text-sm" onClick={handleCancel}>
               Cancelar
             </button>
 
-            <button
-              className="cursor-pointer"
-              onClick={() => {
-                handleRename();
-                setIsRenaming(false);
-                setNewName("");
-              }}
-            >
+            <button className="cursor-pointer text-sm" onClick={handleRename}>
               Salvar
             </button>
           </div>
         ) : (
           <button
-            className="cursor-pointer"
-            onClick={() => {
-              setNewName(project.name);
-              setIsRenaming(true);
-            }}
+            className="cursor-pointer text-sm"
+            onClick={handleStartRename}
           >
             Renomear
           </button>
